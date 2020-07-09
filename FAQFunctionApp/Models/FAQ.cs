1e512d72e6ce00b33aab1a8202bacfab44d6ed6d
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-07-08 20:28:10 -0400

    Configurando JSON

---
+++ FAQFunctionApp/Models/FAQ.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FAQFunctionApp.Models
+{
+    class FAQ
+    {
+        public string Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+
+}
