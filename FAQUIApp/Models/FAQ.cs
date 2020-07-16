b06f12a12bcfd892c1622a9d8831fc7b5877db43
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-07-15 21:31:30 -0400

    Interfaz de faq creada..

---
+++ FAQUIApp/Models/FAQ.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FAQUIApp.Models
+{
+    public class FAQ
+    {
+        [Required(ErrorMessage ="Este campo no puede estar vacio.")]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+        public string Question { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+        public string Answer { get; set; }
+
+
+        public FAQ()
+        {
+            Id = string.Empty;
+            Question = string.Empty;
+            Answer = string.Empty;
+        }
+    }
+}
